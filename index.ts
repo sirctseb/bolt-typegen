762ca2c0f5be14d600c2c52701b1c4efdb2faf5d
Christopher Best <bestchris@gmail.com>
2021-06-02 22:01:27 -0400

    set up project for compilation

--- index.ts
+++ index.ts
@@ -2,10 +2,8 @@ import bolt from 'firebase-bolt';
 import SimpleBoltSchema from './lib/SimpleBoltSchema';
 import renderTypeScript from './lib/renderTypeScript';
 
-export default {
-  typescript: (boltString: string) => {
-    const parsed = bolt.parse(boltString);
-    const schema = new SimpleBoltSchema(parsed.schema);
-    return renderTypeScript(schema);
-  },
+export const generateTypes = (boltString: string) => {
+  const parsed = bolt.parse(boltString);
+  const schema = new SimpleBoltSchema(parsed.schema);
+  return renderTypeScript(schema);
 };
