cf29077d557c7aa94c1a975df89416fb8ba75943
Christopher Best <bestchris@gmail.com>
2021-07-20 22:11:47 -0400

    Add implicit nullability tests

---
+++ samples/implicit-nullability.ts
@@ -0,0 +1,27 @@
+export interface A {
+  mapField?: Record<string, boolean>;
+}
+
+export interface B {
+  mapField?: Record<string, boolean>;
+}
+
+export interface C {
+  nullableField?: boolean;
+}
+export interface CHost {
+  c?: C;
+}
+
+export interface E {
+  nullableField?: boolean;
+}
+
+export interface D {
+  nullableField?: boolean;
+  e?: E;
+}
+
+export interface DHost {
+  d?: D;
+}
