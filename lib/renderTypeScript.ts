24208193364bb836592febb6df115b0d04dfdc77
Christopher Best <bestchris@gmail.com>
2021-08-18 23:12:47 -0400

    Wrap business logic in a class

--- lib/renderTypeScript.ts
+++ lib/renderTypeScript.ts
@@ -12,153 +12,171 @@ const BOLT_BUILTIN_TO_NATIVE: Record<string, ts.KeywordTypeSyntaxKind> = {
 
 const NATIVE_TYPES = ['Boolean', 'Number', 'String'];
 
-const isOptional = (expression: ExpType): expression is ExpUnionType => {
-  return isUnionType(expression) && expression.types.some((type) => isSimpleType(type) && type.name === 'Null');
-};
-
-const hasProperties = (schema: Schema): boolean => {
-  return schema.properties && Object.keys(schema.properties).length > 0;
-};
-
-const isNative = (expression: ExpType) => isSimpleType(expression) && NATIVE_TYPES.includes(expression.name);
-
-const extendsNative = (schema: Schema) =>
-  (isUnionType(schema.derivedFrom) && schema.derivedFrom.types.some(isNative)) || isNative(schema.derivedFrom);
-
 const factory = ts.factory;
 
-const translateSimpleTypeExpression = (
-  builtin: string
-): ts.KeywordTypeNode<ts.KeywordTypeSyntaxKind> | ts.TypeReferenceNode => {
-  if (Object.keys(BOLT_BUILTIN_TO_NATIVE).includes(builtin)) {
-    return factory.createKeywordTypeNode(BOLT_BUILTIN_TO_NATIVE[builtin]);
+class AstTranslator {
+  constructor(private readonly manifest: Record<string, Schema>) {}
+
+  public GenerateDeclarationNodeArray(): ts.NodeArray<ts.TypeAliasDeclaration | ts.InterfaceDeclaration> {
+    return factory.createNodeArray(
+      Object.entries(this.manifest).map(([name, schema]) => this.translateTopLevelTypeDeclaration(name, schema))
+    );
   }
-  return factory.createTypeReferenceNode(factory.createIdentifier(builtin), undefined);
-};
 
-const translateMapExpression = (expression: ExpGenericType): ts.TypeReferenceNode => {
-  return factory.createTypeReferenceNode(factory.createIdentifier('Record'), [
-    factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
-    translateTypeExpression(expression.params[1]),
-  ]);
-};
-
-const translateGenericTypeExpression = (expression: ExpGenericType): ts.TypeReferenceNode => {
-  return expression.name === 'Map'
-    ? translateMapExpression(expression)
-    : factory.createTypeReferenceNode(
-        factory.createIdentifier(expression.name),
-        expression.params.map(translateTypeExpression)
-      );
-};
-
-const translateUnionType = (expression: ExpUnionType): ts.UnionTypeNode => {
-  return factory.createUnionTypeNode(expression.types.map(translateTypeExpression));
-};
-
-const translateTypeExpression = (expression: ExpType): ts.TypeNode => {
-  if (isSimpleType(expression)) {
-    return translateSimpleTypeExpression(expression.name);
+  private isOptional(expression: ExpType): expression is ExpUnionType {
+    return isUnionType(expression) && expression.types.some((type) => isSimpleType(type) && type.name === 'Null');
   }
-  if (isGenericType(expression)) {
-    return translateGenericTypeExpression(expression);
-  }
-  return translateUnionType(expression);
-};
 
-const translatePropertyDeclaration = (name: string, definition: ExpType) => {
-  let modifiedDefinition = definition;
-  if (isOptional(definition)) {
-    modifiedDefinition = {
-      ...definition,
-      types: definition.types.filter((type) => !isSimpleType(type) || type.name !== 'Null'),
-    };
+  private hasProperties(schema: Schema): boolean {
+    return schema.properties && Object.keys(schema.properties).length > 0;
   }
-  return factory.createPropertySignature(
-    /* modifiers */ undefined,
-    name,
-    isOptional(definition) ? factory.createToken(ts.SyntaxKind.QuestionToken) : undefined,
-    translateTypeExpression(modifiedDefinition)
-  );
-};
 
-const makeExpressionWithTypeArgumentsArrayFromTypeNode = (expression: ExpType): ts.ExpressionWithTypeArguments[] => {
-  if (isSimpleType(expression)) {
-    if (expression.name !== 'Object') {
-      return [factory.createExpressionWithTypeArguments(factory.createIdentifier(expression.name), [])];
+  private isNative(expression: ExpType) {
+    return isSimpleType(expression) && NATIVE_TYPES.includes(expression.name);
+  }
+
+  private extendsNative(schema: Schema) {
+    return (
+      (isUnionType(schema.derivedFrom) && schema.derivedFrom.types.some(this.isNative)) ||
+      this.isNative(schema.derivedFrom)
+    );
+  }
+
+  private translateSimpleTypeExpression(
+    builtin: string
+  ): ts.KeywordTypeNode<ts.KeywordTypeSyntaxKind> | ts.TypeReferenceNode {
+    if (Object.keys(BOLT_BUILTIN_TO_NATIVE).includes(builtin)) {
+      return factory.createKeywordTypeNode(BOLT_BUILTIN_TO_NATIVE[builtin]);
     }
-    return [];
+    return factory.createTypeReferenceNode(factory.createIdentifier(builtin), undefined);
   }
-  if (isGenericType(expression)) {
-    return [
-      factory.createExpressionWithTypeArguments(
-        factory.createIdentifier(expression.name),
-        expression.types.map(translateTypeExpression)
-      ),
-    ];
-  }
-  return expression.types.reduce<ts.ExpressionWithTypeArguments[]>(
-    (result, current) => [...result, ...makeExpressionWithTypeArgumentsArrayFromTypeNode(current)],
-    []
-  );
-};
 
-const translateTypeDeclarationToInterface = (name: string, schema: Schema): ts.InterfaceDeclaration => {
-  const ancestors = makeExpressionWithTypeArgumentsArrayFromTypeNode(schema.derivedFrom);
-  const hasAncestors = ancestors.length > 0;
-  return factory.createInterfaceDeclaration(
-    /* decorators */ undefined,
-    /* modifiers */ [factory.createToken(ts.SyntaxKind.ExportKeyword)],
-    name,
-    /* type parameters */ undefined,
-    /* heritage clause */ hasAncestors
-      ? [
-          factory.createHeritageClause(
-            ts.SyntaxKind.ExtendsKeyword,
-            makeExpressionWithTypeArgumentsArrayFromTypeNode(schema.derivedFrom)
-          ),
-        ]
-      : undefined,
-    Object.entries(schema.properties).map(([name, definition]) => translatePropertyDeclaration(name, definition))
-  );
-};
-const translateTypeDeclarationToTypeAlias = (name: string, schema: Schema): ts.TypeAliasDeclaration => {
-  let typeDefinition: ts.TypeNode;
-  if (hasProperties(schema)) {
-    typeDefinition = factory.createIntersectionTypeNode([
-      translateTypeExpression(schema.derivedFrom),
-      factory.createTypeLiteralNode(
-        Object.entries(schema.properties).map(([name, definition]) => translatePropertyDeclaration(name, definition))
-      ),
+  private translateMapExpression(expression: ExpGenericType): ts.TypeReferenceNode {
+    return factory.createTypeReferenceNode(factory.createIdentifier('Record'), [
+      factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+      this.translateTypeExpression(expression.params[1]),
     ]);
-  } else {
-    typeDefinition = translateTypeExpression(schema.derivedFrom);
   }
-  return factory.createTypeAliasDeclaration(
-    undefined /* decorators */,
-    [factory.createToken(ts.SyntaxKind.ExportKeyword)],
-    name,
-    undefined,
-    typeDefinition
-  );
-};
 
-const translateTopLevelTypeDeclaration = (
-  name: string,
-  schema: Schema
-): ts.InterfaceDeclaration | ts.TypeAliasDeclaration => {
-  const nativeExtension = extendsNative(schema);
-  if (nativeExtension) {
-    return translateTypeDeclarationToTypeAlias(name, schema);
+  private translateGenericTypeExpression(expression: ExpGenericType): ts.TypeReferenceNode {
+    return expression.name === 'Map'
+      ? this.translateMapExpression(expression)
+      : factory.createTypeReferenceNode(
+          factory.createIdentifier(expression.name),
+          expression.params.map(this.translateTypeExpression.bind(this))
+        );
   }
-  return translateTypeDeclarationToInterface(name, schema);
-};
+
+  private translateUnionType(expression: ExpUnionType): ts.UnionTypeNode {
+    return factory.createUnionTypeNode(expression.types.map(this.translateTypeExpression.bind(this)));
+  }
+
+  private translateTypeExpression(expression: ExpType): ts.TypeNode {
+    if (isSimpleType(expression)) {
+      return this.translateSimpleTypeExpression(expression.name);
+    }
+    if (isGenericType(expression)) {
+      return this.translateGenericTypeExpression(expression);
+    }
+    return this.translateUnionType(expression);
+  }
+
+  private translatePropertyDeclaration(name: string, definition: ExpType): ts.PropertySignature {
+    let modifiedDefinition = definition;
+    if (this.isOptional(definition)) {
+      modifiedDefinition = {
+        ...definition,
+        types: definition.types.filter((type) => !isSimpleType(type) || type.name !== 'Null'),
+      };
+    }
+    return factory.createPropertySignature(
+      /* modifiers */ undefined,
+      name,
+      this.isOptional(definition) ? factory.createToken(ts.SyntaxKind.QuestionToken) : undefined,
+      this.translateTypeExpression(modifiedDefinition)
+    );
+  }
+
+  private makeExpressionWithTypeArgumentsArrayFromTypeNode(expression: ExpType): ts.ExpressionWithTypeArguments[] {
+    if (isSimpleType(expression)) {
+      if (expression.name !== 'Object') {
+        return [factory.createExpressionWithTypeArguments(factory.createIdentifier(expression.name), [])];
+      }
+      return [];
+    }
+    if (isGenericType(expression)) {
+      return [
+        factory.createExpressionWithTypeArguments(
+          factory.createIdentifier(expression.name),
+          expression.types.map(this.translateTypeExpression.bind(this))
+        ),
+      ];
+    }
+    return expression.types.reduce<ts.ExpressionWithTypeArguments[]>(
+      (result, current) => [...result, ...this.makeExpressionWithTypeArgumentsArrayFromTypeNode(current)],
+      []
+    );
+  }
+
+  private translateTypeDeclarationToInterface(name: string, schema: Schema): ts.InterfaceDeclaration {
+    const ancestors = this.makeExpressionWithTypeArgumentsArrayFromTypeNode(schema.derivedFrom);
+    const hasAncestors = ancestors.length > 0;
+    return factory.createInterfaceDeclaration(
+      /* decorators */ undefined,
+      /* modifiers */ [factory.createToken(ts.SyntaxKind.ExportKeyword)],
+      name,
+      /* type parameters */ undefined,
+      /* heritage clause */ hasAncestors
+        ? [
+            factory.createHeritageClause(
+              ts.SyntaxKind.ExtendsKeyword,
+              this.makeExpressionWithTypeArgumentsArrayFromTypeNode(schema.derivedFrom)
+            ),
+          ]
+        : undefined,
+      Object.entries(schema.properties).map(([name, definition]) => this.translatePropertyDeclaration(name, definition))
+    );
+  }
+
+  private translateTypeDeclarationToTypeAlias(name: string, schema: Schema): ts.TypeAliasDeclaration {
+    let typeDefinition: ts.TypeNode;
+    if (this.hasProperties(schema)) {
+      typeDefinition = factory.createIntersectionTypeNode([
+        this.translateTypeExpression(schema.derivedFrom),
+        factory.createTypeLiteralNode(
+          Object.entries(schema.properties).map(([name, definition]) =>
+            this.translatePropertyDeclaration(name, definition)
+          )
+        ),
+      ]);
+    } else {
+      typeDefinition = this.translateTypeExpression(schema.derivedFrom);
+    }
+    return factory.createTypeAliasDeclaration(
+      undefined /* decorators */,
+      [factory.createToken(ts.SyntaxKind.ExportKeyword)],
+      name,
+      undefined,
+      typeDefinition
+    );
+  }
+
+  private translateTopLevelTypeDeclaration(
+    name: string,
+    schema: Schema
+  ): ts.InterfaceDeclaration | ts.TypeAliasDeclaration {
+    const nativeExtension = this.extendsNative(schema);
+    if (nativeExtension) {
+      return this.translateTypeDeclarationToTypeAlias(name, schema);
+    }
+    return this.translateTypeDeclarationToInterface(name, schema);
+  }
+}
 
 const render = (root: Record<string, Schema>): string => {
   const printer = ts.createPrinter();
-  const declarations = factory.createNodeArray(
-    Object.entries(root).map(([name, schema]) => translateTopLevelTypeDeclaration(name, schema))
-  );
+  const translator = new AstTranslator(root);
+  const declarations = translator.GenerateDeclarationNodeArray();
   return printer.printList(
     ts.ListFormat.SourceFileStatements,
     declarations,
