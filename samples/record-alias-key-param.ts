6fde4e4f584f0b9ea438a56656ffc4c68a66a2ac
Christopher Best <bestchris@gmail.com>
2021-08-26 21:01:57 -0400

    add more record type param tests

--- samples/record-alias-key-param.ts
+++ samples/record-alias-key-param.ts
@@ -1,1 +1,4 @@
 export type RecordAlias<K extends symbol | string | number, V> = Record<K, V>;
+export interface RecordAliasStringKey<K extends symbol | string | number, V> extends Record<string, V> {}
+export type StringAlias = string;
+export interface RecordAliasStringAliasKey<V> extends Map<StringAlias, V> {}
