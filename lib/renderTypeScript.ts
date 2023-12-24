be97358990aaf7ed9c709cd020a0340f7127679d
Christopher Best <bestchris@gmail.com>
2023-12-23 22:47:26 -0500

    add doc comments on translator methods

--- lib/renderTypeScript.ts
+++ lib/renderTypeScript.ts
@@ -19,24 +19,41 @@ const factory = ts.factory;
 class AstTranslator {
   constructor(private readonly manifest: Record<string, Schema>) {}
 
+  /**
+   * Return the top-level interface or type declarations for the Bolt Schemas
+   */
   public GenerateDeclarationNodeArray(): ts.NodeArray<ts.TypeAliasDeclaration | ts.InterfaceDeclaration> {
     return factory.createNodeArray(
       Object.entries(this.manifest).map(([name, schema]) => this.translateTopLevelTypeDeclaration(name, schema))
     );
   }
 
+  /**
+   * Return true iff the expression is a union and one of the values is Null
+   */
   private isOptional(expression: ExpType): expression is ExpUnionType {
     return isUnionType(expression) && expression.types.some((type) => isSimpleType(type) && type.name === 'Null');
   }
 
+  /**
+   * Return true iff the schema has any properties
+   */
   private hasProperties(schema: Schema): boolean {
     return schema.properties && Object.keys(schema.properties).length > 0;
   }
 
+  /**
+   * Return true iff the expression is just one of the entries in NATIVE_TYPES.
+   * These cannot be extended with a TypeScript interface that declared properties.
+   */
   private isNative(expression: ExpType) {
     return isSimpleType(expression) && NATIVE_TYPES.includes(expression.name);
   }
 
+  /**
+   * Map non-generic type expressions to TypeScript equivalents. Either keywords
+   * (e.g., Boolean, Number) or just identifiers like MyType.
+   */
   private translateSimpleTypeExpression(
     builtin: string
   ): ts.KeywordTypeNode<ts.KeywordTypeSyntaxKind> | ts.LiteralTypeNode | ts.TypeReferenceNode {
@@ -51,6 +68,9 @@ class AstTranslator {
     return factory.createTypeReferenceNode(factory.createIdentifier(builtin), undefined);
   }
 
+  /**
+   * Map<KeyType, ValueType> => Record<KeyType, ValueType>
+   */
   private translateMapExpression(expression: ExpGenericType): ts.TypeReferenceNode {
     return factory.createTypeReferenceNode(factory.createIdentifier('Record'), [
       this.translateTypeExpression(expression.params[0]),
@@ -58,6 +78,11 @@ class AstTranslator {
     ]);
   }
 
+  /**
+   * Handles the two generic expression cases:
+   * 1. Map<K, V> (handled by translateMapExpression)
+   * 2. MyCustomGenericType<T1, T2, T3>
+   */
   private translateGenericTypeExpression(expression: ExpGenericType): ts.TypeReferenceNode {
     return expression.name === 'Map'
       ? this.translateMapExpression(expression)
@@ -67,10 +92,19 @@ class AstTranslator {
         );
   }
 
+  /**
+   * Maps unions, recursively translating the member value types
+   */
   private translateUnionType(expression: ExpUnionType): ts.UnionTypeNode {
     return factory.createUnionTypeNode(expression.types.map(this.translateTypeExpression.bind(this)));
   }
 
+  /**
+   * Handle type expressions. They fall into three categories:
+   * 1. Simple: non-generic. Just a built-in type or a name of a declared type.
+   * 1. Generic: MyType<T>
+   * 1. Union: A | B | C<T>
+   */
   private translateTypeExpression(expression: ExpType): ts.TypeNode {
     if (isSimpleType(expression)) {
       return this.translateSimpleTypeExpression(expression.name);
@@ -81,11 +115,23 @@ class AstTranslator {
     return this.translateUnionType(expression);
   }
 
+  /**
+   * Looks like Map<KeyType, ValueType> or ValueType[]
+   */
   private isBoltMap(expression: ExpType): boolean {
-    // both Map<Anything, AnythingElse> and Anything[] are parsed to a Map
+    // both Map<Anything, AnythingElse> and Anything[] are parsed by firebase to a Map
     return isGenericType(expression) && expression.name === 'Map';
   }
 
+  /**
+   * Take a schema like MyType<T1, T2, T3> extends ParentType<T1> { property1: T1 }
+   * and a specification for the type parameters { T1: SomeActualType, T2: Number },
+   * and produce a schema with the type parameters replaced by their specializations.
+   *
+   * We do this so we can determine if an instantiated type can be null or the properties
+   * can be null, and in turn whether the corresponding TypeScript declarations should be
+   * optional.
+   */
   private specializeSchema(schema: Schema, specializations: Record<string, ExpType>): Schema {
     // specialize properties
     const properties: TypeParams = {};
@@ -103,11 +149,16 @@ class AstTranslator {
     };
   }
 
-  // bolt does not allow a type that extends only native types and declares properties
-  // so we don't have to worry about detecting that cant-assign-anything case and return true here
-  // therefore, assumptions:
-  // 1. if the schema has properties, it can be an object and we return true if every field can take null
-  // 2. if the schema has no properties, then we can be null if derived is valueCanBeNull
+  /**
+   * Detect whether the expression refers to a type that is declared by the user
+   * and can take a Null value, factoring in type specialization.
+   *
+   * Bolt does not allow a type that extends only native types and declares properties
+   * so we don't have to worry about detecting that cant-assign-anything case and return true here
+   * therefore, assumptions:
+   * 1. if the schema has properties, it can be an object and we return true if every field can take null
+   * 2. if the schema has no properties, then we can be null if derived is valueCanBeNull
+   */
   private isUserDefinedNullableType(expression: ExpType): boolean {
     if ((isSimpleType(expression) || isGenericType(expression)) && this.manifest[expression.name]) {
       let schema = this.manifest[expression.name];
@@ -122,16 +173,18 @@ class AstTranslator {
     return false;
   }
 
-  // returns true if a property typed with type expression can ever have a null value
-  // there are no actual null values in firebase, the property is just not there, so in typescript
-  // we have to mark the field as optional. this can happen if the expression is a union including Null
-  // myField: String | Null; => myField?: string;
-  // it can also happen that the expression is or is a union that includes a type that does not have any
-  // required keys. E.g. Map<String, Boolean> or a type with declared properties, all of which are
-  // unions including Null
+  /**
+   * Returns true if a property typed with type expression can ever have a null value
+   * there are no actual null values in firebase, the property is just not there, so in typescript
+   * we have to mark the field as optional. this can happen if the expression is a union including Null
+   * myField: String | Null; => myField?: string;
+   * it can also happen that the expression is or is a union that includes a type that does not have any
+   * required keys. E.g. Map<String, Boolean> or a type with declared properties, all of which are
+   * unions including Null
+   */
   private valueCanBeNull(expression: ExpType): boolean {
-    // the expression is a union containing Null
     return (
+      // the expression is a union containing Null
       this.isOptional(expression) ||
       // the expression is a Bolt syntax convenience for a map
       this.isBoltMap(expression) ||
@@ -142,6 +195,9 @@ class AstTranslator {
     );
   }
 
+  /**
+   * Take a Bolt property declaration and produce the equivalent TypeScript property
+   */
   private translatePropertyDeclaration(name: string, definition: ExpType): ts.PropertySignature {
     let modifiedDefinition = definition;
     if (this.isOptional(definition)) {
@@ -160,6 +216,10 @@ class AstTranslator {
     );
   }
 
+  /**
+   * Translate a Bolt heritage clause (extends SomethingOrOther<T> | OtherThing) to TypeScript.
+   * TODO I don't remember why this translation would be different than translateTypeExpression.
+   */
   private makeExpressionWithTypeArgumentsArrayFromTypeNode(expression: ExpType): ts.ExpressionWithTypeArguments[] {
     if (isSimpleType(expression)) {
       if (expression.name !== 'Object') {
@@ -181,6 +241,9 @@ class AstTranslator {
     );
   }
 
+  /**
+   * Return true iff the expression looks like Map<K, V> given `param` K.
+   */
   private isMapWithGivenKeyTypeArg(expression: ExpGenericType, param: string) {
     return (
       expression.name === 'Map' &&
@@ -216,7 +279,7 @@ class AstTranslator {
   }
 
   // e.g. type A<K> extends Map<K, String> {}
-  // in typescript, we have to constrain a type param if it is passed as the key type arg
+  // in TypeXcript, we have to constrain a type param if it is passed as the key type arg
   // to a Record
   private typeParameterUsedInRecordKey(param: string, schema: Schema): boolean {
     const parent = schema.derivedFrom;
@@ -234,6 +297,9 @@ class AstTranslator {
     return false;
   }
 
+  /**
+   * Translate Bolt type parameters to TypeScript equivalent
+   */
   private translateTypeParameters(schema: Schema): ts.TypeParameterDeclaration[] | undefined {
     return (
       schema.params &&
@@ -253,6 +319,9 @@ class AstTranslator {
     );
   }
 
+  /**
+   * Handle translation of a type declaration when it can be expressed as an interface
+   */
   private translateTypeDeclarationToInterface(name: string, schema: Schema): ts.InterfaceDeclaration {
     const ancestors = this.makeExpressionWithTypeArgumentsArrayFromTypeNode(schema.derivedFrom);
     const hasAncestors = ancestors.length > 0;
@@ -273,6 +342,10 @@ class AstTranslator {
     );
   }
 
+  /**
+   * Handle translation of a type declaration when it cannot be expressed an interface,
+   * so we use a type declaration instead.
+   */
   private translateTypeDeclarationToTypeAlias(name: string, schema: Schema): ts.TypeAliasDeclaration {
     let typeDefinition: ts.TypeNode;
     if (this.hasProperties(schema)) {
