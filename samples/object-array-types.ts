96c74397c4926439e711f4ee1cd6b849453a86c7
Christopher Best <bestchris@gmail.com>
2024-01-02 21:32:52 -0800

    add test cases for FirebaseArray types

---
+++ samples/object-array-types.ts
@@ -0,0 +1,41 @@
+export type FirebaseArray<K extends string, T> = Record<K, T> | T[];
+export type WithoutArrays<Type> = {
+  [Property in keyof Type]: Type[Property] extends FirebaseArray<
+    infer K extends string,
+    infer T
+  >
+    ? Record<K, WithoutArrays<T>>
+    : Type[Property];
+};
+export type RecordAlias<K extends string, V> = FirebaseArray<K, V>;
+export type RecordAliasStringKey<V> = FirebaseArray<string, V>;
+export type StringAlias = string;
+export type RecordAliasStringAliasKey<V> = FirebaseArray<StringAlias, V>;
+export type ArraySyntaxAlias<V> = FirebaseArray<string, V>;
+export type SecondOrderArraySyntaxAlias<V> = ArraySyntaxAlias<V>;
+export type WithChildren<V> = Record<string, V> & {
+  child: string;
+};
+export type WithOptionalChildren<V> = FirebaseArray<string, V> & {
+  child?: string;
+};
+export type WithBoth<V> = Record<string, V> & {
+  child?: string;
+  child2: string;
+};
+export interface UsedByWithMapInTypeParameter<V> {
+  child: V;
+}
+export interface WithMapInTypeParameter<V> {
+  child?: UsedByWithMapInTypeParameter<FirebaseArray<string, V>>;
+}
+export type AncestorUnion<V> = (string | Record<string, V>) & {
+  child: string;
+};
+export type AncestorUnionOptional<V> = (string | FirebaseArray<string, V>) & {
+  child?: string;
+};
+export type RecordAliasDescendant<K extends string, V> = RecordAlias<K, V>;
+export type RecordAliasOrStringDescendant<K extends string, V> =
+  | string
+  | RecordAlias<K, V>;
